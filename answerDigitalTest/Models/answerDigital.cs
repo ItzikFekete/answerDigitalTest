a7badff5481316170bf27fa7a0d8517ae2021b12
ItzikFekete <34029678+ItzikFekete@users.noreply.github.com>
2018-05-08 15:51:28 +0100

    Add project files.

---
+++ answerDigitalTest/Models/answerDigital.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace answerDigitalTest.Models
+{
+    public class answerDigital
+    {
+        public int personId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAuthorised { get; set; }
+        public int ColourId { get; set; }
+        public string ColourName { get; set; }
+    }
+}
